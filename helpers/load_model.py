7031c60457ca2c5c4f477fb113a23ada87c82f0a
Charlie Boxall <boxallcharlie@gmail.com>
2025-04-14 22:11:43 +0100

    altered load_model

--- helpers/load_model.py
+++ helpers/load_model.py
@@ -13,6 +13,14 @@ def load_ac_model(checkpoint, TRAINING=False):
             print("Model loaded succesfully.")
             return model, tokenizer, feature_extractor, None
         
+        elif checkpoint == "ac_models/finetuned_models/ftwhispertiny-PLUS":
+            print("IN")
+            model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
+            tokenizer = WhisperTokenizer.from_pretrained(checkpoint, language="en", task="transcribe")
+            feature_extractor = WhisperFeatureExtractor.from_pretrained(checkpoint)
+            print("Model loaded succesfully.")
+            return model, tokenizer, feature_extractor, None
+        
         elif checkpoint == "MU-NLPC/whisper-tiny-audio-captioning":
             model = WhisperForConditionalGeneration.from_pretrained(checkpoint)
             if TRAINING:
