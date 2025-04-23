5e9b2e68c4812f96814413617f2a415f1c9ee003
Charlie Boxall <boxallcharlie@gmail.com>
2025-04-23 13:36:53 +0100

    added example style prefixes to example use

--- example_use.py
+++ example_use.py
@@ -6,6 +6,26 @@ from gifa_library import GIFA
 audio_file_to_process = """C:/GIFA-acoustic/test_audios/test3.mp3"""
 
 # Optional: Define any prefixes or other parameters
+"""
+--------------------- EXAMPLE STYLE PREFIXES: ---------------------
+["Cartoon", "Pixel Art", 
+"Sketch", "Line Art", 
+"Vector Art", "Comic Book Art", 
+"Graphic Novel Art", "Minimalist Poster", 
+"Geometric Design", "Children's Book Illustration", 
+"Concept Art", "Low Poly", 
+"Isometric Art", "Glitch Art", 
+"Vaporwave Aesthetic", "Synthwave Aesthetic", 
+"ASCII Art", "Digital Painting", 
+"Matte Painting", "Watercolour", 
+"Charcoal Sketch", "Chalk Drawing", 
+"Woodcut Print Style", "Engraving Style", 
+"Stained Glass", "Psychedelic Art", 
+"Fantasy Art", "Sci-Fi Art", 
+"Cyberpunk Art", "Steampunk Art", 
+"Grunge Aesthetic", "Art Deco Poster", 
+"Art Nouveau Poster"]
+"""
 style_prefixes_to_add = ["cinematic", "wide angle"]
 output_folder = "generated_images"
 image_width = 1024
